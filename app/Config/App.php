bad459c870e009414f253b40f1cef90ec2a9e899
naseem umar masaid <naseem1masaid@gmail.com>
2024-11-25 22:47:01 +0700

    main

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/perikanan/public/';
+    public string $baseURL = 'http://localhost/RebrandingWeb/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
