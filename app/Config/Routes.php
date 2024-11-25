e84dd9f92901fdc7e8a6afcf020622142ad07b03
naseem umar masaid <naseem1masaid@gmail.com>
2024-11-25 22:15:54 +0700

    main

---
+++ app/Config/Routes.php
@@ -0,0 +1,23 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+if (is_file(SYSTEMPATH . 'Config/Routes.php')) {
+    require SYSTEMPATH . 'Config/Routes.php';
+}
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+$routes->setAutoRoute(true);
+
+$routes->get('/', 'Home::index');
+
+if (is_file(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
+    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+}
