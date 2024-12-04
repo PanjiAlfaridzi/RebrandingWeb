784102a6b03586a3d167e65568b16320e577aa4d
Ivan Maulana Anggita <62920144+ivananggita043@users.noreply.github.com>
2024-12-04 10:26:56 +0700

    main

--- app/Views/v_dokumennav.php
+++ app/Views/v_dokumennav.php
@@ -352,7 +352,7 @@
             <p>&copy; <?= date('Y') ?> Kominfo Semarang. All Rights Reserved.</p>
         </div>
     </footer><!-- End Footer -->
-
+    
     <div id="preloader"></div>
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
